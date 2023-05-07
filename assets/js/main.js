e34c0083686f960b62bf2b2b87490fe49f97981f
zCoder <cycs.ingenieria@gmail.com>
2023-05-07 17:28:17 -0400

    reingeniería

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
-const container = document.querySelector('#container')
+const container = document.querySelector("#container");
 
 const llamadaApi = async () => {
-    const peticion = await fetch('https://rickandmortyapi.com/api/character')
-    const datos = await peticion.json()
-    const {results} = datos
-    results.map( item => {
-        const personajeDiv = document.createElement('div')
-        const imagen = document.createElement('img')
-        const nombre = document.createElement('h2')
-        const enlace = document.createElement('a')
-        enlace.href = `personaje.html?id=${item.id}`
-        imagen.src = item.image
-        imagen.classList.add('image')
-        nombre.innerHTML = item.name
-        personajeDiv.appendChild(imagen)
-        enlace.appendChild(imagen)
-        personajeDiv.appendChild(nombre)
-        personajeDiv.appendChild(enlace)
-        container.appendChild(personajeDiv)
-    })
-    console.log(results)
-}
+  const peticion = await fetch("https://rickandmortyapi.com/api/character");
+  const datos = await peticion.json();
+  const { results } = datos;
+  results.map((item) => {
+    const personajeDiv = document.createElement("div");
+    const imagen = document.createElement("img");
+    const nombre = document.createElement("h2");
+    const enlace = document.createElement("a");
+    enlace.href = `personaje.html?id=${item.id}`;
+    imagen.src = item.image;
+    imagen.classList.add("image");
+    nombre.innerHTML = item.name;
+    personajeDiv.appendChild(imagen);
+    enlace.appendChild(imagen);
+    personajeDiv.appendChild(nombre);
+    personajeDiv.appendChild(enlace);
+    container.appendChild(personajeDiv);
+  });
+  console.log(results); 
+};
 
-llamadaApi()
+llamadaApi();
